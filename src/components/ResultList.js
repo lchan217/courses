dd3d517ed9564df14cbc38f26f70dd2d2698e1c8
Lizzie <lchan217@gmail.com>
2020-04-10 11:11:38 -0700

    add link and button for more details

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
-const ResultList = props => {
-  return (
-    <div>
-      {props.course.title} -{" "}
-      <Link to={`${props.course.courseId}`}>More Detail</Link>
-    </div>
-  );
-};
+class ResultList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.course.title} -{" "}
+        <Link
+          to={{
+            pathname: `${this.props.course.courseId}`
+          }}
+        >
+          <Button>More Detail</Button>
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default ResultList;
