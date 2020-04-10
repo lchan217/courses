d2aeedbfada27030623c76b3c0cede084388ce65
Lizzie <lchan217@gmail.com>
2020-04-10 14:45:24 -0700

    add initial format to show page

--- src/components/ShowCourse.js
+++ src/components/ShowCourse.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Icon, Image } from "semantic-ui-react";
 
 const ShowCourse = props => {
-  const { title } = props.location.state.course;
+  const {
+    title,
+    author,
+    shortDescription,
+    duration,
+    durationPeriod,
+    imgUrl,
+    level,
+    url
+  } = props.location.state.course;
   return (
     <div>
       <h1>{title}</h1>
+      <h3>By: {author}</h3>
+      <Image src={imgUrl} size='medium' />
+      <div>
+        {shortDescription}{" "}
+        <a href={url}>
+          {" "}
+          <Icon name='angle double right'></Icon>
+        </a>
+      </div>
+      <div>
+        Time: {duration} {durationPeriod}
+      </div>
+      <div>Level: {level}</div>
     </div>
   );
 };
