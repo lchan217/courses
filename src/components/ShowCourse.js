ab15924097a41429c33b68e9e6dbb79ff0cbf09a
Lizzie <lchan217@gmail.com>
2020-04-12 16:29:01 -0700

    add home button

--- src/components/ShowCourse.js
+++ src/components/ShowCourse.js
@@ -1,35 +1,44 @@
-import React from "react";
-import { Icon, Image } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Container, Button, Icon, Image } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
 
-const ShowCourse = props => {
-  const {
-    title,
-    author,
-    shortDescription,
-    duration,
-    durationPeriod,
-    imgUrl,
-    level,
-    url
-  } = props.location.state.course;
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h3>By: {author}</h3>
-      <Image src={imgUrl} size='medium' />
-      <div>
-        {shortDescription}{" "}
-        <a href={url}>
-          {" "}
-          <Icon name='angle double right'></Icon>
-        </a>
-      </div>
-      <div>
-        Time: {duration} {durationPeriod}
-      </div>
-      <div>Level: {level}</div>
-    </div>
-  );
-};
+class ShowCourse extends Component {
+  previousPage = () => {
+    window.location.href = "/";
+  };
+  render() {
+    const {
+      title,
+      author,
+      shortDescription,
+      duration,
+      durationPeriod,
+      imgUrl,
+      level,
+      url
+    } = this.props.location.state.course;
 
+    return (
+      <Container>
+        <Button floated='right' onClick={this.previousPage}>
+          Back
+        </Button>
+        <h1>{title}</h1>
+        <h3>By: {author}</h3>
+        <Image src={imgUrl} size='medium' />
+        <div>
+          {shortDescription}{" "}
+          <a href={url}>
+            {" "}
+            <Icon name='angle double right'></Icon>
+          </a>
+        </div>
+        <div>
+          Time: {duration} {durationPeriod}
+        </div>
+        <div>Level: {level}</div>
+      </Container>
+    );
+  }
+}
 export default ShowCourse;
